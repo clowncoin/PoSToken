980b6da317cd6f648e91a83f2120706e012e21f0
Martin Kainzbauer <m.kainzbauer@xtools.at>
2018-07-23 11:03:44 +0200

    updated truffle.js

--- truffle.js
+++ truffle.js
@@ -1,9 +1,19 @@
 module.exports = {
-  networks: {
-    development: {
-      host: "localhost",
-      port: 8545,
-      network_id: "*" // Match any network id
-    }
-  }
+    // See <http://truffleframework.com/docs/advanced/configuration>
+    // to customize your Truffle configuration!
+	networks: {
+		development: {
+			host: "127.0.0.1",
+			port: "7545",
+			network_id: "*", // match any network id
+			gasPrice: 1000000000
+		},
+		rinkeby: {
+			host: "localhost",
+			port: 8545,
+			network_id: 4,
+			gas: 2000000,
+			gasPrice: 5000000000 // 5 Gwei
+		}
+	}
 };
